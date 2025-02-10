ad546812b18dded55790e13038ffaa2151e46807
Rafael Barreto Braidotte <164931045+rafael-inforlube@users.noreply.github.com>
2025-02-10 06:44:12 -0300

    Inclui autenticação de usuários

--- index.js
+++ index.js
@@ -13,6 +13,50 @@ app.launcher = function() {
     document.getElementById("Print").addEventListener("click", function() { window.print(); });
     document.getElementById("User").innerHTML = user.name;
     document.getElementById("Store").innerHTML = store.name;
+
+    // Inicializa os componentes de primeiro plano
+    loader.build();
+    // Obtem as credenciais do usuário
+    const { search } = window.location;
+    const params = new URLSearchParams(search);
+    const token = params.get('token');
+    const path = "https://api.lubraxmaissystem.com/Lubrax.Mais.Api/api/User/getUserToken?token=" + token;
+    const make = {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'data-culture': 'pt-BR',
+            'Authorization': 'Bearer ' + token
+        }
+    };
+    loader.show("Realizando autenticação no sistema");
+    fetch(path, make)
+    .then(async response => {
+        if (response.ok == true) {
+            const data = await response.json();
+            localStorage.setItem('apps', JSON.stringify(data.apps));
+            localStorage.setItem('user', JSON.stringify(data.user));
+            localStorage.setItem('store', JSON.stringify(data.store));
+            localStorage.setItem('token', data.token);
+            return;
+        } else {
+            if (response.status === 401) {
+                loader.chat("Token inválido, redirecionando para a página de login!");
+                window.location.href = "https://auth.lubraxmaissystem.com/";
+                return;
+            };
+        };
+        return response.json();
+    })
+    .catch(error => { snackbar.show(error.message.text); })
+    .finally(() => { loader.hide(); });
+
+
+
+
+
+
     // Renderiza o QR Code
     if (user != undefined && user != null && user != "" && user.id != undefined && user.id != null && user.id != "") {
         let box = document.getElementById("QRCode");
